2dea16014660f758e1b29b1ec9c425e833fbe87f
cezkuj <kujawskicezary@wp.pl>
2018-04-18 18:08:30 +0000

    Added template for db operations

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
+//import "github.com/cezkuj/gostuff/some_pkg"
 
-import "github.com/cezkuj/gostuff/some_pkg"
 func main() {
 	//chanells()
 	//scope()
 	//test_interfaces()
-        some_pkg.Printy()
+	//some_pkg.Printy()
+        test_db()
 }
